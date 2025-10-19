a5914bb92a3d4e6a30a4228c9dedddd4ad7523d4
Yannis_Skopoulis <scopoulis@admin.grnet.gr>
2025-10-19 22:28:38 +0300

    page92-added json api for albums

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -3,6 +3,18 @@ defmodule Tunez.Music do
     otp_app: :tunez,
     extensions: [AshJsonApi.Domain, AshPhoenix]
 
+  json_api do
+    routes do
+      base_route "/artists", Tunez.Music.Artist do
+        get :read
+        index :search
+        post :create
+        patch :update
+        delete :destroy
+      end
+    end
+  end
+
   forms do
     form :create_album, args: [:artist_id]
   end
@@ -28,18 +40,6 @@ defmodule Tunez.Music do
       define :get_album_by_id, action: :read, get_by: :id
       define :update_album, action: :update
       define :destroy_album, action: :destroy
-    end
-  end
-
-  json_api do
-    routes do
-      base_route"/artists", Tunez.Music.Artist do
-        get :read
-        index :search
-        post :create
-        patch :update
-        delete :destroy
-      end
     end
   end
 end
