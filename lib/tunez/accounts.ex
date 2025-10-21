6e6a4836a3f17c3771313f1f127423be688b4e09
Yannis_Skopoulis <scopoulis@admin.grnet.gr>
2025-10-21 18:32:02 +0300

    page 123 - before introducing policies

--- lib/tunez/accounts.ex
+++ lib/tunez/accounts.ex
@@ -1,9 +1,17 @@
 defmodule Tunez.Accounts do
-  use Ash.Domain,
-    otp_app: :tunez
+  use Ash.Domain, otp_app: :tunez, extensions: [AshJsonApi.Domain]
 
   resources do
     resource Tunez.Accounts.Token
     resource Tunez.Accounts.User
   end
+
+  json_api do
+    routes do
+      base_route"/users", Tunez.Accounts.User do
+          post :register_with_password, route: "/register"
+      end
+    end
+  end
+
 end
