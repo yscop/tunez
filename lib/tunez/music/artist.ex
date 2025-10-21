9a015eba0f1bf00abbb0beaccc6e76dd668571b8
Yannis_Skopoulis <scopoulis@admin.grnet.gr>
2025-10-21 16:15:00 +0300

    page119-before adding magic link strategy

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -7,14 +7,14 @@ defmodule Tunez.Music.Artist do
 
   graphql do
     type :artist
+
     filterable_fields [
-      :album_count, 
-      :cover_image_url, 
+      :album_count,
+      :cover_image_url,
       :inserted_at,
-      :latest_album_year_released, 
+      :latest_album_year_released,
       :updated_at
     ]
-    
   end
 
   json_api do
